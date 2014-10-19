ce5b611e3a1470e371a0e9f373f0375b45f2aede
zhangbo0716 <13811962378@163.com>
2014-10-19 22:12:09 +0800

    add ItemBase and .ignore

---
+++ src/com/yao/hadoop/ItemBasedTest.java
@@ -0,0 +1,11 @@
+package com.yao.hadoop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangbo
+ * Date: 14-10-19
+ * Time: обнГ10:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemBasedTest {
+}
